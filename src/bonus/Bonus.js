687587abc1181e30d0cd403a52f011e6b40d4749
Kathy Coffee <klcoffee8807@gmail.com>
2021-12-05 21:10:42 -0500

    Create React File. Clear App.js file Create Exercise.js file. Complete exercise. Create Bonusfile. Complete bonus question.

---
+++ src/bonus/Bonus.js
@@ -0,0 +1,7 @@
+function Bonus () {
+    return(
+    <h1>Bonus Component</h1>
+    );
+}
+
+export default Bonus;
---
+++ src/exercise/Exercise.js
@@ -0,0 +1,26 @@
+import Bonus from "../bonus/Bonus";
+
+// function Exercise ({good, bad }){
+
+function Exercise ({good = "Great", bad = "Not Great"})
+{
+    return (
+        <div>
+            {/* 
+            <h1>Great</h1>
+            <h1>Not Great</h1> 
+            */}
+
+            <h1>{good}</h1>
+            <h1>{bad}</h1>
+            <Bonus />
+        </div>
+    )
+}
+
+
+
+
+
+
+export default Exercise;
